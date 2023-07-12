2c9ec5fd1613368d37547a3a8e06178a226fc22d
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-07-13 05:28:26 +0530

    Update 9.c

--- 9.c
+++ 9.c
@@ -19,86 +19,167 @@ fi
 echo "$str1 and $str2"
 
 // oddchild
-    #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <string.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <sys/wait.h>
-#include <fcntl.h>
+   // program to print n odd numbers using shm_open and mmap system calls.
+//child
+    
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/shm.h>
+#include<sys/mman.h>
 
-int main(int argc,char *argv[])
-{
-int k=1,odd=1,n;
-void *ptr;
-int shmid = shmget((key_t)151515,4096,IPC_CREAT|0666);
-ptr = shmat(shmid,NULL,0666);
-printf("CHILD\n");
-sscanf(argv[1],"%d",&n);
-n = atoi(argv[1]);
-while(k<=n)
-{
-sprintf(ptr,"%d ",odd);
-printf("%d ",odd);
-ptr+=strlen(ptr);
-k++;
-odd+=2;
-}
-return 0;
+int main(int argc, char *argv[]) {
+    int n, i=1;
+    void *shmptr;
+    int shmid = shm_open("OS", O_CREAT|O_RDWR, 0666);
+    ftruncate(shmid, 4096);
+    shmptr = mmap(0, 4096, PROT_WRITE, MAP_SHARED, shmid, 0);
+    printf("\nChild Printing:\n");
+    n = atoi(argv[1]);
+    
+    while(n != 0) {
+        if(i%2 != 0) {
+            sprintf(shmptr, "%d ", i);
+            printf("%d ", i);
+            shmptr += strlen(shmptr);
+            n--;
+        }
+        i++;
+    }
+
+    return 0;
 }
 
-//odd parent
+//parent
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <string.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <fcntl.h>
-int main(int argc, char *argv[])
-{
-int i;
-pid_t pid;
-const int SIZE=4096;
-void *ptr;
-int shmid;
-if(argc>1)
-{
-sscanf(argv[1],"%d",&i);
-if(i<1)
-{
-printf("Error Input : %d\n",i);
-return 0;
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/shm.h>
+#include<sys/mman.h>
+
+int main(int argc, char *argv[]) {
+    int n, i;
+    pid_t pid;
+    void *shmptr;
+    int shmid;
+
+    if (argc > 1) {
+        sscanf(argv[1], "%d", &n);
+        if(n<1) {
+            printf("\nWrong input given!!\n");
+            return 0;
+        }
+    } else {
+        printf("\nN is not passed in the command line!!\n");
+        exit(0);
+    }
+
+    pid = fork();
+    if(pid == 0) {
+        //This is the child process
+        execlp("./odd", "odd", argv[1], NULL);
+    } else if(pid > 0) {
+        wait(NULL);
+        printf("\n[PARENT] Child process completed\n");
+        shmid = shm_open("OS", O_RDONLY, 0666);
+        ftruncate(shmid, 4096);
+        shmptr = mmap(0, 4096, PROT_READ, MAP_SHARED, shmid, 0);
+        printf("\nParent Printing:\n");
+        printf("%s\n", (char *)shmptr);
+        shm_unlink("OS");
+    }
+
+    return 0;
 }
+
+//with shmget
+//child
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/shm.h>
+#include<sys/mman.h>
+
+int main(int argc, char *argv[]) {
+    int i = 1, n;
+    void *shmptr;
+    int shmid = shmget((key_t)1122, 4096, 0666);
+    shmptr = shmat(shmid, NULL, 0666);
+
+    printf("\nChild Printing:\n");
+    n = atoi(argv[1]);
+
+    while(n != 0) {
+        if(i%2 != 0) {
+            sprintf(shmptr, "%d ", i);
+            printf("%d ", i);
+            shmptr += strlen(shmptr);
+            n--;
+        }
+        i++;
+    }
+
+    shmctl(shmid, IPC_RMID, NULL);
+    return 0;
 }
-else
-{
-printf("Error : Not passing N in Command Line\n");
-exit(0);
-}
-pid = fork();
-if(pid==0)
-{
-execlp("./odd","odd",argv[1],NULL);
-}
-else if(pid >0)
-{
-wait(NULL);
-printf("\nPARENT : Child Completed\n");
-shmid = shmget((key_t)151515,4096,0666);
-ptr = shmat(shmid,NULL,0666);
-printf("Parent printing\n");
-printf("%s",(char *)ptr);
-printf("\n");
-shmdt(ptr);
-}
-return 0;
+//parent
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/shm.h>
+#include<sys/mman.h>
+
+int main(int argc, char *argv[]) {
+    int i, n;
+    pid_t pid;
+    const int SIZE = 4096;
+    void *shmptr;
+    int shmid = shmget((key_t)1122, 4096, 0666|IPC_CREAT);
+    shmptr = shmat(shmid, NULL, 0666);
+
+    if(argc > 1) {
+        sscanf(argv[1], "%d", &i);
+        if(i < 1) {
+            printf("\nWrong input is passed:\n");
+            return 0;
+        }
+    } else {
+        printf("\nN is not passed in the command line\n");
+        return 1;
+    }
+
+    pid = fork();
+    if(pid == 0) {
+        //This is the child process part
+        execlp("./odd", "odd", argv[1], NULL);
+    } else if(pid > 0) {
+        wait(NULL);
+        printf("\n[PARENT] Child completed\n");
+        printf("\nParent printing:\n");
+        printf("%s\n", (char *)shmptr);
+        shmdt(shmptr); 
+    } 
+
+    return 0;
 }

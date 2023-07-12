c9180b522daf2da38f50076bad909c1ccb89221c
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-07-12 22:14:30 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,43 +1,42 @@
 // 5.a
 
+// to demonstrate the use of I/O system calls for file I/O( read, write, open, lseek)
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
-int main(int argc, char *argv[])
-{
+#define BUFFER_SIZE 30
+int main() {
 int fd,n;
-char buffer[30];
-if(argc<2)
+char buffer[BUFFER_SIZE];
+fd = open("file.c", O_RDWR);
+
+if (fd == -1) 
 {
-printf("File name not entered\n");
+printf("Failed to open file.\n");
 exit(0);
 }
-fd=open(argv[1],O_RDWR);
-if(fd==-1)
-{
-printf("Failed to open file\n");
-exit(0);
-}
-printf("Reading first 10 characters from file\n");
-n = read(fd,buffer,10);
-write(0,buffer,n);
-printf("Skipping 5 characters from current position in file\n");
-lseek(fd,5,SEEK_CUR);
-n = read(fd,buffer,10);
-write(0,buffer,n);
-printf("Going to 10 characters before current position in file\n");
-lseek(fd,-10,SEEK_CUR);
-n = read(fd,buffer,10);
-write(0,buffer,n);
-printf("Reading 5 characters before end position in file\n");
-lseek(fd,-5,SEEK_END);
-n = read(fd,buffer,10);
-write(0,buffer,n);
-printf("Going to 3rd character before beginning in file\n");
-lseek(fd,3,SEEK_SET);
-n = read(fd,buffer,10);
-write(0,buffer,n);
+printf("Reading first 10 characters from file: \n");
+n = read(fd, buffer, 10);
+write(1, buffer, n);
+
+printf("\nSkipping 5 characters from current position in the file: \n");
+lseek(fd, 5, SEEK_CUR);
+n = read(fd, buffer, 10);
+write(1, buffer, n);
+ printf("\nGoing 10 characters before the current position in the file: \n");
+lseek(fd, -10, SEEK_CUR);
+n = read(fd, buffer, 10);
+write(1, buffer, n);
+printf("\nGoing to 5th last character in the file: \n");
+lseek(fd, -5, SEEK_END);
+n = read(fd, buffer, 7);
+write(1, buffer, 5);
+printf("\nGoing to the 3rd character in the file: \n");
+lseek(fd, 3, SEEK_SET);
+n = read(fd, buffer, 5);
+write(1, buffer, n);
+  return 0;
 }
 
 //FCFS

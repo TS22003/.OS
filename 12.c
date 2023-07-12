630b6bdf19a47b3d4b68e9d8591dbab69bf5a11f
VISHRUTH V S <88739731+VishruthVS@users.noreply.github.com>
2023-07-12 19:53:35 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,3 +1,22 @@
+//largest
+
+#!/bin/bash
+if [ $1 -gt $2 ]
+then if [ $1 -gt $3 ]
+then echo "$1 is greatest"
+fi
+fi
+if [ $2 -gt $1 ]
+then if [ $2 -gt $3 ]
+then echo "$2 is greatest"
+fi
+fi
+if [ $3 -gt $1 ]
+then if [ $3 -gt $2 ]
+then echo "$3 is greatest"
+fi
+fi
+
 // Solution for first readers writers problem using mutex and semaphore. There are 10 readers and 5 writers to demonstrate the solution. 
 
 
